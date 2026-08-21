b952f92f868082f46577a87f73167f3b4ae6ce7d
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-08-21 12:48:39 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import { Oval } from 'react-loader-spinner'
 
 // import SimpleLightbox from "simplelightbox";
